621395e9f039cf1f658c5771cadc0b1403e45834
camara_aicha <aicha.ca@outlook.fr>
2024-09-10 14:00:36 +0200

    changement de la function showhide mainteant ça rajoute le texte dans la balise et non a l'exterieur

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     function showhide() {
-        // Sélectionner l'article avec l'ID 'citation'
         let article = document.getElementById('citation');
 
-        // Vérifier si l'article contient déjà du texte
         if (article.textContent.trim() === '') {
-            // Si l'article est vide, ajouter le texte
             article.textContent = "L'important n'est pas la chute, mais l'atterrissage.";
         } else {
-            // Si l'article a déjà du texte, le vider
             article.textContent = '';
         }
     }
 
-    // Attacher l'événement 'click' au bouton pour appeler la fonction showhide
     document.getElementById('button').addEventListener('click', showhide);
 });
